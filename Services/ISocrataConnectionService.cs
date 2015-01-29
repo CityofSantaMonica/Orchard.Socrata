5c2eea1e0d5723abd00c284958952e1f63621941
Kegan Maher <kegan.maher@smgov.net>
2015-01-29 15:39:31 -0800

    use SODA for making connections to a host

---
+++ Services/ISocrataConnectionService.cs
@@ -0,0 +1,27 @@
+﻿using CSM.Socrata.Models;
+using Orchard;
+using SODA;
+
+namespace CSM.Socrata.Services
+{
+    /// <summary>
+    /// Service definition for making Socrata connections using the defined site settings.
+    /// </summary>
+    public interface ISocrataConnectionService : IDependency
+    {
+        /// <summary>
+        /// Get the Socrata settings defined in the current site.
+        /// </summary>
+        SocrataSettingsPart GetSettings();
+
+        /// <summary>
+        /// Create a new <see cref="SODA.SodaClient"/> for read-access using the defined site settings.
+        /// </summary>
+        SodaClient GetAnonymousClient();
+
+        /// <summary>
+        /// Create a new <see cref="SODA.SodaClient"/> for read/write-access using the defined site settings.
+        /// </summary>
+        SodaClient GetAuthenticatedClient();
+    }
+}
---
+++ Services/SocrataConnectionService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CSM.Socrata.Models;
+using Orchard;
+using Orchard.ContentManagement;
+using SODA;
+
+namespace CSM.Socrata.Services
+{
+    public class SocrataConnectionService : ISocrataConnectionService
+    {
+        private readonly IOrchardServices _orchardServices;
+
+        public SocrataConnectionService(IOrchardServices orchardServices)
+        {
+            _orchardServices = orchardServices;
+        }
+
+        public SocrataSettingsPart GetSettings()
+        {
+            return _orchardServices.WorkContext.CurrentSite.As<SocrataSettingsPart>();
+        }
+
+        public SodaClient GetAnonymousClient()
+        {
+            var settings = GetSettings();
+
+            if (settings == null)
+                throw new InvalidOperationException("No Socrata settings have been defined.");
+
+            return new SodaClient(settings.Host, settings.AppToken);
+        }
+
+        public SodaClient GetAuthenticatedClient()
+        {
+            var settings = GetSettings();
+
+            if (settings == null)
+                throw new InvalidOperationException("No Socrata settings have been defined.");
+
+            if (String.IsNullOrEmpty(settings.Username) || String.IsNullOrEmpty(settings.Password))
+                throw new InvalidOperationException("No Socrata user credentials have been defined.");
+
+            return new SodaClient(settings.Host, settings.AppToken, settings.Username, settings.Password);
+        }
+    }
+}
